f95437a7bf69612b3ab24f9054f5486a81610418
Lorik Limani <limani.lorik@ibu.edu.mk>
2026-03-02 22:33:09 +0100

    Add linear search implementation for string comparison
    Implement linear search for strings using strcmp.

---
+++ src/search1.c
@@ -0,0 +1,20 @@
+/*
+WEEK 1 — TASK 4 (Linear Search - Strings)
+
+Goal:
+Practice string comparison.
+
+Task:
+1. Create an array of strings (names).
+2. Ask the user for a name.
+3. Search the array.
+4. Use strcmp() to compare strings.
+5. Print:
+Found
+or
+Not found
+
+Rules:
+- You must use strcmp().
+- Do NOT use == for string comparison.
+*/
