012ed4aa550ac2101c9c3fb64cf61b6bff4c3ec4
Lorik Limani <limani.lorik@ibu.edu.mk>
2026-03-02 22:32:49 +0100

    Add linear search implementation for integers
    Implement linear search for integers in an array.

---
+++ src/search0.c
@@ -0,0 +1,19 @@
+/*
+WEEK 1 — TASK 3 (Linear Search - Integers)
+
+Goal:
+Practice arrays and linear search.
+
+Task:
+1. Create an array of integers.
+2. Ask the user for a number.
+3. Search the array manually (no library search).
+4. If found print:
+Found
+   Otherwise print:
+Not found
+
+Rules:
+- You must use a loop.
+- Do NOT use any built-in search function.
+*/
